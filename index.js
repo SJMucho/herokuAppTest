9103cf3f16072f49264c5cc924e172e335690449
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-11-09 21:29:27 -0500

    cors, final touches to index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/", (_, res) => {
   res.json({ data: "API is accounted for" });
